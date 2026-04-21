f848fc3e36e132eb8cba372ab63f979efebf2899
Marc3935 <ma3935@gmail.com>
2026-04-21 09:26:31 +0200

    Added ILogin

---
+++ EksamenConsoleApp/EksamenConsoleApp/ILogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EksamenConsoleApp
+{
+    public interface ILogin
+    {
+
+
+        public void Login()
+        {
+            Console.WriteLine("Login Succesfull");
+        }
+    }
+}
