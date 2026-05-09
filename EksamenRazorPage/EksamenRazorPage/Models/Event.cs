ee5594493e490c40cfaa6a0e2ce8e34966c62d03
Younas Latif <yol001@edu.zealand.dk>
2026-05-09 16:31:07 +0200

    Tilføjet mørkt tema, rettet navbar login-link, opdateret forside med søgefelt og pokémon kort, tilføjet properties til Event model og oprettet ny migration

--- EksamenRazorPage/EksamenRazorPage/Models/Event.cs
+++ EksamenRazorPage/EksamenRazorPage/Models/Event.cs
@@ -2,5 +2,11 @@
 {
     public class Event
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string? Url { get; set; }
+        public bool IsActive { get; set; }
     }
 }
