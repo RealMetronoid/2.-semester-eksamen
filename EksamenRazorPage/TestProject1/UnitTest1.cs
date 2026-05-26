e0ef7ff50c231f894b75c960b832ed6253f029ae
Marc3935 <ma3935@gmail.com>
2026-05-26 07:35:41 +0200

    unit tests

--- EksamenRazorPage/TestProject1/UnitTest1.cs
+++ EksamenRazorPage/TestProject1/UnitTest1.cs
@@ -1,4 +1,10 @@
-﻿namespace TestProject1
+﻿using EksamenRazorPage;
+using Login.Pages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+
+namespace TestProject1
 {
     public class UnitTest1
     {
@@ -7,5 +13,38 @@
         {
 
         }
+
+        [Fact]
+        public async Task PasswordNotMatching_ReturnsPageResult()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<PokemonContext>()
+                .UseInMemoryDatabase(databaseName: "RegisterTestDb")
+                .Options;
+
+            var context = new PokemonContext(options);
+
+            var model = new RegisterModel(context)
+            {
+                Username = "Marcus",
+                DisplayName = "Marc3935",
+                Email = "a@gmail.com",
+                Password = "abc",
+                ConfirmPassword = "xyz"
+            };
+
+            // Simulate invalid ModelState
+            model.ModelState.AddModelError("ConfirmPassword", "Passwords do not match");
+
+            // Act
+            var result = await model.OnPostCreateUser();
+
+            // Assert
+            Assert.IsType<PageResult>(result);
+        }
+
+
+
+
     }
 }
